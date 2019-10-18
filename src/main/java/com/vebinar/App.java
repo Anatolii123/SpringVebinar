0527b21cf93070322d4de0e9a8773f765c232fe9
AKusakin <anatolii.kusakin1234@gmail.com>
2019-10-18 12:13:05 +0500

    MVC3Commit

--- src/main/java/com/vebinar/App.java
+++ src/main/java/com/vebinar/App.java
@@ -1,7 +1,14 @@
 package com.vebinar;
 
+import com.vebinar.config.SpringConfig;
+import com.vebinar.service.TestBean;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class App {
     public static void main(String[] args) {
+
+         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
+        TestBean bean = context.getBean(TestBean.class);
 
     }
 }
