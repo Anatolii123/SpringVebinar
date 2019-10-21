bfe6a1369db8d9d13ff2ce6de2704a78b62db49c
AKusakin <anatolii.kusakin1234@gmail.com>
2019-10-21 11:44:24 +0500

    DataBase2Commit

--- src/main/java/com/vebinar/dao/UserDaoImpl.java
+++ src/main/java/com/vebinar/dao/UserDaoImpl.java
@@ -1,5 +1,6 @@
 package com.vebinar.dao;
 
+import com.sun.xml.internal.bind.v2.model.core.ID;
 import com.vebinar.entity.User;
 import com.vebinar.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,8 +18,32 @@ public class UserDaoImpl implements UserDao {
     }
 
     @Override
+    public void save(User user) {
+        String sql = "INSERT INTO SPRING_PRACTICE_USER (NAME, EMAIL, AGE) VALUES (?,?,?)";
+        jdbcTemplate.update(sql, user.getName(), user.getEmail(), user.getAge());
+    }
+
+    @Override
+    public User getById(int id) {
+        String sql = "SELECT * FROM SPRING_PRACTICE_USER WHERE ID = ?";
+        return jdbcTemplate.queryForObject(sql, new UserMapper(), id);
+    }
+
+    @Override
     public List<User> findAll() {
         String sql = "SELECT * FROM SPRING_PRACTICE_USER";
         return jdbcTemplate.query(sql, new UserMapper());
+    }
+
+    @Override
+    public void update(User user) {
+        String sql = "UPDATE SPRING_PRACTICE_USER SET NAME=?, EMAIL=?,AGE=? WHERE ID=?";
+        jdbcTemplate.update(sql,user.getName(), user.getEmail(), user.getAge(), user.getId());
+    }
+
+    @Override
+    public void delete(int id) {
+        String sql = "DELETE FROM SPRING_PRACTICE_USER WHERE ID=?";
+        jdbcTemplate.update(sql, id);
     }
 }
